d8c7dc54a2ad222a2bcda9c4a1b2b1a9fd7057b6
Ilya <karpov.ilja@gmail.com>
2024-04-09 20:58:44 +0300

    Books data; db task

---
+++ query.sql
@@ -0,0 +1,19 @@
+--
+-- Dokumentācija:
+-- https://www.w3schools.com/sql/default.asp
+-- vai
+-- https://sqlzoo.net/wiki/SQL_Tutorial
+-- Palaist kodu:
+-- https://sqlfiddle.com/sqlite/online-compiler (SQLite vai MySQL)
+-- or
+-- https://sqlite.org/fiddle/ 
+--
+
+-- Uzdevums: uzrakstīt zemāk minētus skripts un saglabāt šajā failā
+
+-- Izveidot tabulu atbilstoši faila books.json struktūrai (CREATE)
+-- Ierakstīt 10 jebkuru grāmatu ierakstus jaunā tabulā (INSERT)
+-- Atlasīt tikai grāmatu nosaukumu, ISBN un lapu skaitu (SELECT)
+-- Atlasīt grāmatu ar konkrētu ISBN (SELECT, WHERE)
+-- Sakārtot tabulu pēc lapu skaita, no lielāka uz mazāko (SELECT, ORDER BY)
+
